aa2f4be741f70be74c0df09a393543600f6b21ca
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-07-03 14:24:44 +0900

    feat: export mutation phase coordinates

--- src/eco_genetic_warning_extensions/__init__.py
+++ src/eco_genetic_warning_extensions/__init__.py
@@ -1,11 +1,14 @@
 """Protocol-locked utilities for eco-genetic warning extensions."""
 
 from .asymmetric_mutation import AsymmetricMutation, mutate_frequency
+from .mutation_coordinates import MutationCoordinates, primary_phase_grid
 from .protocol001 import CalibrationCandidate, select_protocol_001_domain
 
 __all__ = [
     "AsymmetricMutation",
     "CalibrationCandidate",
+    "MutationCoordinates",
     "mutate_frequency",
+    "primary_phase_grid",
     "select_protocol_001_domain",
 ]
