28b1dfc8edeed86cbc829cba378b077f334b77a3
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-07-10 14:19:39 +0900

    feat: export protocol 002 mutation-slot fixtures

--- src/eco_genetic_warning_extensions/__init__.py
+++ src/eco_genetic_warning_extensions/__init__.py
@@ -3,6 +3,11 @@
 from .asymmetric_mutation import AsymmetricMutation, mutate_frequency
 from .mutation_coordinates import MutationCoordinates, primary_phase_grid
 from .protocol001 import CalibrationCandidate, select_protocol_001_domain
+from .protocol002_mutation_slot_fixture import (
+    MutationSlotFixture,
+    protocol002_mutation_slot_trajectory,
+    symmetric_slot_bridge_differences,
+)
 from .protocol002_stage0 import stage0_certificate, write_stage0_certificate
 from .protocol002_upstream_adapter import (
     PINNED_UPSTREAM_LIFECYCLE,
@@ -15,13 +20,16 @@ __all__ = [
     "AsymmetricMutation",
     "CalibrationCandidate",
     "MutationCoordinates",
+    "MutationSlotFixture",
     "PINNED_UPSTREAM_LIFECYCLE",
     "apply_protocol002_mutation",
     "apply_symmetric_bridge",
     "mutate_frequency",
     "primary_phase_grid",
+    "protocol002_mutation_slot_trajectory",
     "select_protocol_001_domain",
     "stage0_certificate",
     "symmetric_bridge_coordinate",
+    "symmetric_slot_bridge_differences",
     "write_stage0_certificate",
 ]
